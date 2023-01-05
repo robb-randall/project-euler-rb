771dce0686a37738fa9a81bd9cde45d3efc38292
Robb Randall <robb.randall@gmail.com>
2023-01-05 17:05:23 -0600

    refactored problems files

--- lib/project_euler.rb
+++ lib/project_euler.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-Dir[File.join(__dir__, 'project_euler', '*.rb')].sort.each { |file| require file }
+require 'project_euler/helpers.rb'
+Dir[File.join(__dir__, 'project_euler', 'problems', '*.rb')].sort.each { |file| require file }
 
 # Project Euler helper functions
 module ProjectEuler
