054536a9ca3f8c2bbfa610fd7812b5f6777b85ff
Robb Randall <robb.randall@gmail.com>
2023-01-05 16:11:57 -0600

    added #problem005

---
+++ lib/project_euler/problem_005.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Problem 005
+module Problems
+  def problem005(min_value, max_value)
+    # TODO: This solution needs optimization
+    current_value = max_value
+    divisors = (min_value..max_value).to_a
+
+    while current_value += max_value
+      return current_value if divisible?(current_value, *divisors)
+    end
+  end
+end
--- spec/project_euler_problems_spec.rb
+++ spec/project_euler_problems_spec.rb
@@ -27,5 +27,15 @@ RSpec.describe ProjectEuler do
         expect(described_class.problem004(100, 999)).to eq(906_609)
       end
     end
+
+    describe '#problem005' do
+      it 'returns the smallest possible number divisible by all numbers between 1 and 10' do
+        expect(described_class.problem005(1, 10)).to eq(2_520)
+      end
+
+      it 'returns the smallest possible number divisible by all numbers between 1 and 20' do
+        expect(described_class.problem005(1, 20)).to eq(232_792_560)
+      end
+    end
   end
 end
