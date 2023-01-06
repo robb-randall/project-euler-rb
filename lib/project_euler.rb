768f0f2b0c1a1483a53787f5ec192be78d448eb4
Robb Randall <robb.randall@gmail.com>
2023-01-05 20:45:59 -0600

    refactored test cases

--- lib/project_euler.rb
+++ lib/project_euler.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
-require 'project_euler/helpers.rb'
+require 'project_euler/helpers'
 Dir[File.join(__dir__, 'project_euler', 'problems', '*.rb')].sort.each { |file| require file }
 
 # Project Euler helper functions
 module ProjectEuler
-  VERSION = 1.0
-
   include Helpers
   extend Problems
 end
